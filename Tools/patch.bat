51e5b5adf41c771be496b093c8183379d1487f5b
IDerr <ibrahim@derraz.fr>
2017-05-23 21:29:01 +0200

    Update files + patch

--- Tools/patch.bat
+++ Tools/patch.bat
@@ -1,3 +1,4 @@
-arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\Patch\patch1" "..\Release\Patch\patch1.xp3" mahoyo
-arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\Patch\patch2" "..\Release\Patch\patch2.xp3" mahoyo
-arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\Patch\patch3" "..\Release\Patch\patch3.xp3" mahoyo
+arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\patch1" "..\Release\patch1.xp3" mahoyo
+arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\patch2" "..\Release\patch2.xp3" mahoyo
+arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\patch3" "..\Release\patch3.xp3" mahoyo
+set /p DUMMY=Hit ENTER to continue...
